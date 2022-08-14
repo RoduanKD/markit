e0d274f26b54fa66aa42655af19a7cb66f87ed79
Aubada10 <obada.tr@gmail.com>
2022-08-14 17:08:31 +0300

    make relationship between role and permission

--- database/migrations/2022_08_14_135716_create_permissions_role_table.php
+++ database/migrations/2022_08_14_135716_create_permissions_role_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('permissions_role', function (Blueprint $table) {
+        Schema::create('permission_role', function (Blueprint $table) {
             $table->foreignId('permission_id')->constrained();
             $table->foreignId('role_id')->constrained();
         });
