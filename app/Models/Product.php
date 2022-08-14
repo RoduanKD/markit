2ed798f9e28514e251cd5876a1cf137652f9758b
ghassan_mg <ghassan.mgsyr@gmail.com>
2022-08-14 17:37:44 +0300

    add traits for media & soft-delete

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory,InteractsWithMedia,SoftDeletes;
 
     public function Category()
     {
