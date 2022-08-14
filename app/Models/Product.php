da1459302af2ebbdddadda922540607735920621
Leenja <102230205+Leenja@users.noreply.github.com>
2022-08-14 17:58:30 +0300

    Product Model

---
+++ app/Models/Product.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    public function owner()
+    {
+        return $this->belongsTo('User' , 'owner_id');
+    }
+
+    public function price()
+    {
+        return $this->belongsTo(Currency::class);
+    }
+
+    public function areas()
+    {
+        return $this->belongsTo(Area::class);
+    }
+
+    public function media()
+    {
+        return $this->hasMany(Media::class);
+    }
+}
