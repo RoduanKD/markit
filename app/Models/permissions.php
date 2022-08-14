41d45cbbd41004d5814981aa53274cd7654496fa
Aubada10 <obada.tr@gmail.com>
2022-08-14 17:10:55 +0300

    make relationship

--- app/Models/permissions.php
+++ app/Models/permissions.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class permissions extends Model
+class Permissions extends Model
 {
     use HasFactory;
 }
