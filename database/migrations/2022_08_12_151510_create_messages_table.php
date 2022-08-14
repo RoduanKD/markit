899721ca5a5be00f53f7e2e0c12d461293e27a93
tamara <tamshehadeh@gmail.com>
2022-08-14 16:50:18 +0300

    add phone

--- database/migrations/2022_08_12_151510_create_messages_table.php
+++ database/migrations/2022_08_12_151510_create_messages_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email');
+            $table->string('phone');
             $table->text('content');
             $table->softDeletes();
             $table->timestamps();
