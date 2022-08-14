7a3b5b19b731eca2e3bdecc4e9916ce079b55b85
Leenja <102230205+Leenja@users.noreply.github.com>
2022-08-14 17:58:08 +0300

    Create products and orders migrations

---
+++ database/migrations/2022_08_13_133907_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('buyer_id');
+            $table->foreignId('seller_id');
+            $table->foreignId('product_id');
+            $table->integer('payment_method')->unsigned();
+            $table->integer('status')->unsigned();
+            $table->integer('quantity')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
