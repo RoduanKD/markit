914b229a9b26bac4efa3fd7dcd2326b69a550ca5
Leenja <102230205+Leenja@users.noreply.github.com>
2022-08-21 14:08:32 +0300

    seeding products and orders

---
+++ database/seeders/OrderSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Order;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Order::create(['id' => '1' ,
+                     'buyer_id' => '1' ,
+                     'seller_id' => '5' ,
+                     'product_id' => '1',
+                     'payment_method' => '2',
+                     'status' => '2',
+                     'quantity' => '2'] ,
+                     ['id' => '2' ,
+                     'buyer_id' => '3' ,
+                     'seller_id' => '2' ,
+                     'product_id' => '2',
+                     'payment_method' => '1',
+                     'status' => '1',
+                     'quantity' => '1']);
+    }
+}
