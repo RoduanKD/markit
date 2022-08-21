0b5b8e8dfe9db8192a5683acd82c70c576f67fe5
mashlahmahmod <mashlahmahmod@gmail.com>
2022-08-21 16:20:19 +0300

    Category Resource Route

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,15 @@ Route::middleware('auth')->group(function () {
     Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
     Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
 });
+
+
+Route::resource('categories', CategoryController::class);
+
+
+
+
+
+
+
+
+
