1a1f1aadcb5cdc98674c34a3caa89441e3c74af1
Aubada10 <obada.tr@gmail.com>
2022-09-02 18:31:09 +0300

    create pemissions seeder

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -15,12 +15,29 @@ class PermissionSeeder extends Seeder
      */
     public function run()
     {
-        Permission::create(['name'=>'create_post']);
-        Permission::create(['name'=>'edit_post']);
-        Permission::create(['name'=>'create_user']);
+        // permissions for super admin
+            Permission::create(['name'=>'CRUD_Admins']);
+            Permission::create(['name'=>'update_website_settings']);
+
+        // permissions for  admin
+        Permission::create(['name'=>'read_customers']);
+        Permission::create(['name'=>'update_customers']);
+        Permission::create(['name'=>'read_products']);
+        Permission::create(['name'=>'update_products']);
+        Permission::create(['name'=>'CRUD_countries']);
+        Permission::create(['name'=>'CRUD_cities']);
+        Permission::create(['name'=>'CRUD_areas']);
+
+        // permissions for  moderator
+        Permission::create(['name'=>'create_posts']);
+        Permission::create(['name'=>'edit_posts']);
         Permission::create(['name'=>'delete_posts']);
-        Permission::create(['name'=>'write_comment']);
-        Permission::create(['name'=>'block_user']);
-        Permission::create(['name'=>'buy_products']);
+        Permission::create(['name'=>'edit_profile']);
+        // permissions for customer
+        Permission::create(['name'=>'create_products']);
+        Permission::create(['name'=>'update_products']);
+        // permissions for delivery
+        Permission::create(['name'=>'read_delivery_tasks']);
+        Permission::create(['name'=>'update_delivery_tasks']);
     }
 }
